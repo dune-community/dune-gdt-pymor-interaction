b4fb001cddf54836173864f0afe6baede09ec20a
Felix Schindler <felix.schindler@wwu.de>
2017-01-29 15:37:59 +0100

    [start_notebook_server] do not source PATH.sh, assume venv loaded

--- start_notebook_server.py
+++ start_notebook_server.py
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-if [[ ! -e PATH.sh ]] ; then
-  echo "Missing PATH.sh, exiting!"
-  exit 1
-fi
-. PATH.sh
-
-export NOTEBOOK_PATH=$BASEDIR/notebooks
+export NOTEBOOK_PATH=$PWD/notebooks
 export NOTEBOOK_PORT=18881
 
 jupyter-notebook --notebook-dir=$NOTEBOOK_PATH --port=$NOTEBOOK_PORT
+
